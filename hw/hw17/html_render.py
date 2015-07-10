7d7ed7e71f89760128f5c7bcd5099a9185f86f82
isaacchen <isaac.chen@gmail.com>
2015-07-10 13:16:15 -0700

    problem 2

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -10,14 +10,59 @@ Python class example.
 
 
 class Element(object):
-    def __init__(self, name="", content=""):
-        self.name = name
+
+    # IND_LEVEL = "    "
+
+    def __init__(self, content='', name='html', indent_level=0):
         self.content = content
+        self.name = name
+        self.indent_level = indent_level
+        # you can define attribute without reference in__init__
+        self.children = [content] if content else []
 
-    def append(self, new_text):
-        self.content += ('\n' + new_text)
+    def append(self, new_child):
+        self.children.append(new_child)
+        # self.content += str(new_child)
 
-    def render(self, filename, ind="    "):
-        filename.write('<html>\n')
-        filename.write(ind + self.content)
-        filename.write('\n</html>')
+    def render(self, outfile, content=''):
+        tag_indent = '    ' * self.indent_level
+        txt_indent = '    ' * (self.indent_level + 1)
+        if (self.name == 'html'):
+            outfile.write('<%s>\n' % self.name)
+        else:
+            outfile.write(('%s<%s>\n') % (tag_indent, self.name))
+
+        for child in self.children:
+            if (type(child) == str):
+                # add nenw content string
+                # outfile.write(indent + Element.IND_LEVEL + child + "\n")
+                outfile.write(txt_indent + child + '\n')
+            else:
+                # add new child node by recursinvely rendering
+                # child.render(outfile, indent + Element.IND_LEVEL)
+                child.render(outfile, txt_indent)
+
+        # outfile.write("%s</%s>\n" % (indent, self.name))
+        if (self.name == 'html'):
+            outfile.write('</%s>' % self.name)
+        else:
+            outfile.write(('%s</%s>\n') % (tag_indent, self.name))
+
+
+class Html(Element):
+    def __init__(self, content='', name='', indent_level=0):
+        Element.__init__(self, content='', name='html', indent_level=0)
+
+    def render(self, outfile, content=''):
+        outfile.write('<!DOCTYPE html>\n')
+        Element.render(self, outfile, '')
+
+
+class Body(Element):
+    def __init__(self, content='', name='', indent_level=0):
+        Element.__init__(self, content='', name='body', indent_level=1)
+
+
+class P(Element):
+    def __init__(self, content, name='', indent_level=0):
+        Element.__init__(self, content, name='p', indent_level=2)
