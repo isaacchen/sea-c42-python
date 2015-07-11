cdd05c0b24cc1080451cde586b55358f8ae3e7b0
isaacchen <isaac.chen@gmail.com>
2015-07-10 17:02:10 -0700

    step 3 tested

--- hw/hw17/html_render.py
+++ hw/hw17/html_render.py
@@ -43,6 +43,11 @@ class Element(object):
         else:
             outfile.write(('%s</%s>\n') % (tag_indent, self.name))
 
+    def OneLineTag(self, outfile, content, name='', indent_level=0):
+        tag_indent = '    ' * indent_level
+        oneline = '%s<%s>%s</%s>\n' % (tag_indent, name, self.content, name)
+        outfile.write(oneline)
+
 
 class Html(Element):
     def __init__(self, content='', name='', indent_level=0):
@@ -61,3 +66,17 @@ class Body(Element):
 class P(Element):
     def __init__(self, content, name='', indent_level=0):
         Element.__init__(self, content, name='p', indent_level=2)
+
+
+class Head(Element):
+    def __init__(self, content='', name='', indent_level=0):
+        Element.__init__(self, content='', name='head', indent_level=1)
+
+
+class Title(Element):
+    def __init__(self, content, name='', indent_level=0):
+        Element.__init__(self, content, name='title', indent_level=2)
+
+    def render(self, outfile, content, name='', indent_level=0):
+        Element.OneLineTag(self, outfile, self.content, name='title',
+                           indent_level=self.indent_level)
